65b625dce6c3320efbf456ad97fa4a52e6ad23d5
PunitAITIAN <punitait2028@gmail.com>
2026-08-10 02:39:57 +0530

    100 . Same Tree

---
+++ Tree/100.SameTree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // base case
+        if(p==NULL && q==NULL){
+            return true;
+        }
+        if((p!=NULL && q==NULL)||(p==NULL && q!=NULL)){
+            return false;
+        }
+        if(p->val!=q->val){
+            return false;
+        }
+        // recusrion dekh lega
+        bool leftans = isSameTree(p->left,q->left);
+
+        bool rightans = isSameTree(p->right,q->right);
+
+        if(rightans==true && leftans==true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
